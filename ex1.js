98e1636704f43378396e321e091e1b4b0bb13e19
ccbaron <christian7baron@gmail.com>
2025-03-11 20:49:20 +0100

    Ejercicio 5

--- ex1.js
+++ ex1.js
@@ -2,7 +2,7 @@
  * Ejercicio 1: Escribe un bucle que muestre por consola 10 veces la frase "Estoy en un bucle!" El ejercicio está CASI bien, pero falta arreglar alguna cosa. Ejecútalo para ver que sucede.
  */
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 10; i++) {
   console.log("Estoy en un bucle!");
 }
 
--- ex2.js
+++ ex2.js
@@ -3,6 +3,10 @@
  * que muestre por el terminal el mensaje "Yo también estoy en un bucle!" ; un total de 20 veces.
  */
 
+for (var i = 0; i < 20; i++) {
+    console.log("Yo tambien estoy en un bucle!");
+  }
+
 /**
  * Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzU2M18yMjIwMzc5Ng
  */
